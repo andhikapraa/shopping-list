4102787430a017bd18b27d7b73f057583852592d
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-09-20 15:41:55 +0700

    Merestriksi Akses Halaman Main

--- main/views.py
+++ main/views.py
@@ -7,8 +7,10 @@ from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     products = Product.objects.all()
 
