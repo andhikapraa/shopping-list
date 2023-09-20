c9ff13bdc04cd0161501da8e827c1984da7fcbf6
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-09-20 15:38:19 +0700

    Membuat Fungsi Logout

--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from main.models import Product
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def show_main(request):
@@ -56,3 +57,16 @@ def register(request):
             return redirect('main:login')
     context = {'form':form}
     return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
