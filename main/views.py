fccb1cf56ddfc2e4a7c466bd61a0d2fc7c936ab7
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-09-20 15:35:51 +0700

    Membuat Fungsi dan Form Registrasi

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from main.forms import ProductForm
 from django.urls import reverse
 from main.models import Product
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
 
 # Create your views here.
 def show_main(request):
@@ -42,3 +44,15 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
